c5ac4793cfdfac2da245f8e2a17251ccc117c3c3
Stellar-security-os <150491881+stellar-security-os@users.noreply.github.com>
2025-06-18 01:48:30 +0200

    Initial setup.

---
+++ app/Http/Controllers/V1/NoteController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+
+    public function create(Request $request) {
+        // ..
+    }
+
+    public function find(Request $request)
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('V1/notecontroller')->group(function () {
+    Route::patch('/update', [\App\Http\Controllers\V1\NoteController::class, 'update']);
+    Route::post('/create', [\App\Http\Controllers\V1\NoteController::class, 'create']);
+    Route::post('/find', [\App\Http\Controllers\V1\NoteController::class, 'find']);
+});
