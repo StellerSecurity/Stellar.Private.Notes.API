a149aa62623d183097e89476aa2042a754734419
Stellar-security-os <150491881+stellar-security-os@users.noreply.github.com>
2025-06-23 19:38:02 +0200

    Implemented find, delete, create, update.

--- app/Http/Controllers/V1/NoteController.php
+++ app/Http/Controllers/V1/NoteController.php
@@ -3,22 +3,93 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Note;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
 
-    public function create(Request $request) {
-        // ..
+    public function create(Request $request): JsonResponse
+    {
+        $user_id = $request->input('user_id');
+        $json_content = $request->input('json_content');
+
+        if(empty($user_id) || empty($json_content)) {
+            return response()->json(['response_code' => 400, 'response_message' => 'User ID or json_content is empty.'], 400);
+        }
+
+        $note = Note::where('user_id', $user_id)->first();
+
+        if($note !== null) {
+            return response()->json(['response_code' => 400, 'response_message' => 'Note info already exists, use UPDATE/PATCH'], 400);
+        }
+
+        Note::create($request->all());
+
+        return response()->json(['response_code' => 200, 'response_message' => 'Note created successfully.'], 200);
+
     }
 
-    public function find(Request $request)
+    public function find(Request $request): JsonResponse
     {
+
+        $user_id = $request->input('user_id');
+
+        if(empty($user_id)) {
+            return response()->json(['response_code' => 400, 'response_message' => 'User ID  is empty.'], 400);
+        }
+
+        $note = Note::where('user_id', $user_id)->first();
+
+        if($note === null) {
+            return response()->json(['response_code' => 400, 'response_message' => 'Note info not found'], 400);
+        }
+
+        return response()->json($note, 200);
 
     }
 
-    public function update(Request $request)
+    public function delete(Request $request): JsonResponse
     {
+
+        $user_id = $request->input('user_id');
+
+        if(empty($user_id)) {
+            return response()->json(['response_code' => 400, 'response_message' => 'User ID  is empty.'], 400);
+        }
+
+        $note = Note::where('user_id', $user_id)->first();
+
+        if($note === null) {
+            return response()->json(['response_code' => 400, 'response_message' => 'Note info not found'], 400);
+        }
+
+        $note->delete();
+
+        return response()->json(['response_code' => 200, 'response_message' => 'Note deleted successfully.'], 200);
+
+    }
+
+    public function update(Request $request): JsonResponse
+    {
+
+        $user_id = $request->input('user_id');
+        $json_content = $request->input('json_content');
+
+        if(empty($user_id) || empty($json_content)) {
+            return response()->json(['response_code' => 400, 'response_message' => 'User ID or json_content is empty.'], 400);
+        }
+
+        $note = Note::where('user_id', $user_id)->first();
+
+        if($note == null) {
+            return response()->json(['response_code' => 400, 'response_message' => 'Note info not found, use CREATE.'], 400);
+        }
+
+        $note->update($request->all());
+
+        return response()->json(['response_code' => 200, 'response_message' => 'Note created successfully.'], 200);
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ Route::prefix('V1/notecontroller')->group(function () {
     Route::patch('/update', [\App\Http\Controllers\V1\NoteController::class, 'update']);
     Route::post('/create', [\App\Http\Controllers\V1\NoteController::class, 'create']);
     Route::post('/find', [\App\Http\Controllers\V1\NoteController::class, 'find']);
+    Route::delete('/delete', [\App\Http\Controllers\V1\NoteController::class, 'delete']);
 });
