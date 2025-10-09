e933b94474b2489cb8d1272eb8f8f9d349f0e7cc
stellar-security-os <stellarsecaps@gmail.com>
2025-10-09 20:41:12 +0200

    Updated download to GET

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/v1/notecontroller/sync-plan', [\App\Http\Controllers\V1\NoteController::class, 'plan']);
 Route::post('/v1/notecontroller/upload',    [App\Http\Controllers\V1\NoteController::class, 'upload'])->middleware(Idempotency::class);
-Route::post('/v1/notecontroller/download',  [App\Http\Controllers\V1\NoteController::class, 'download']);
+Route::get('/v1/notecontroller/download',  [App\Http\Controllers\V1\NoteController::class, 'download']);
