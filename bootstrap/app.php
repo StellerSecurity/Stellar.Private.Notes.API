2d2c9432ea4ba62b0ae0bcc7e20fc247eeff0e8d
stellar-security-os <stellarsecaps@gmail.com>
2025-10-09 20:19:51 +0200

    Removed basic for test.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->use([
-            \App\Http\Middleware\BasicAuthentication::class,
             'idempotency'  => \App\Http\Middleware\Idempotency::class,
         ]);
     })
