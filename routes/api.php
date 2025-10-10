1ee220a017fffb2e25f07c96c1233518f8a24eb5
stellar-security-os <stellarsecaps@gmail.com>
2025-10-10 12:06:19 +0200

    Changed to post- endpoints.

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/v1/notecontroller/sync-plan', [\App\Http\Controllers\V1\NoteController::class, 'plan']);
 Route::post('/v1/notecontroller/upload',    [App\Http\Controllers\V1\NoteController::class, 'upload'])->middleware(Idempotency::class);
-Route::get('/v1/notecontroller/download',  [App\Http\Controllers\V1\NoteController::class, 'download']);
-Route::get('/v1/notecontroller/find',  [App\Http\Controllers\V1\NoteController::class, 'find']);
+Route::post('/v1/notecontroller/download',  [App\Http\Controllers\V1\NoteController::class, 'download']);
+Route::post('/v1/notecontroller/find',  [App\Http\Controllers\V1\NoteController::class, 'find']);
